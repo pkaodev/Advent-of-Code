a2a033c4e601713ba3e0d20752ce736a0ad6a839
Patrik Olsson <polsson.development@gmail.com>
2023-12-04 20:03:08 +0000

    meeeeeeeeeeeeehhhhhhhhhhhhh

---
+++ default-setups/Python/solution.py
@@ -0,0 +1,8 @@
+import os
+
+input_path = os.path.join(os.path.dirname(__file__), 'input')
+
+with open(input_path, 'r', encoding='utf8') as file:
+    data = file.read()
+
+lines = data.split('\n')
