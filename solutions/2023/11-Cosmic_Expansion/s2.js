802f00a33d1a8bcdf59d79da3307efb281cad14f
Patrik Olsson <patrik.olsson@engineering.digital.dwp.gov.uk>
2023-12-11 17:17:50 +0000

    11

--- solutions/2023/11-Cosmic_Expansion/s2.js
+++ solutions/2023/11-Cosmic_Expansion/s2.js
@@ -14,37 +14,29 @@ function isRowEmpty(row) {
 function isColumnEmpty(column) {
     return column.every(x=>x==='.')
 }
-function expandRow(grid, rowI) {
-    grid.splice(rowI, 0, '.'.repeat(grid[0].length))
-
-}
-function expandColumn(grid, columnI) {
-    for (const rowI in grid) {
-        const newRow = grid[rowI].slice(0,columnI) + '.' + grid[rowI].slice(columnI)
-        grid[rowI] = newRow
-    }
-}
-// mutates
 function expandUniverse(grid) {
+    const emptyRows = []
     for (let i=0; i<grid.length; i++) {
         if (isRowEmpty(grid[i])) {
-            expandRow(grid, i)
-            i++
+            emptyRows.push(i)
         }
     }
 
+    const emptyColumns = []
     for (let i=0; i<grid[0].length; i++) {
         const column = []
         for (let y=0; y<grid.length; y++) {
             column.push(grid[y][i])
         }
         if (isColumnEmpty(column)) {
-            expandColumn(grid, i)
-            i++
+            emptyColumns.push(i)
         }
     }
 
-    return grid
+    return {
+        emptyRows,
+        emptyColumns
+    }
 }
 function findGalaxies(grid) {
     const galaxyLocations = []
@@ -57,28 +49,33 @@ function findGalaxies(grid) {
     }
     return galaxyLocations
 }
-function findShortestPaths(galaxyLocations) {
+function findShortestPaths(galaxyLocations, emptyRowsColumns) {
+    const expFact = 1
     const shortestPaths = []
     for (let g1=0; g1<galaxyLocations.length-1; g1++) {
         for (let g2=g1+1; g2<galaxyLocations.length; g2++) {
-            shortestPaths.push(Math.abs(galaxyLocations[g2].x-galaxyLocations[g1].x)+Math.abs(galaxyLocations[g2].y-galaxyLocations[g1].y))
+            const maxX = Math.max(galaxyLocations[g1].x, galaxyLocations[g2].x)
+            const minX = Math.min(galaxyLocations[g1].x, galaxyLocations[g2].x)
+            const maxY = Math.max(galaxyLocations[g1].y, galaxyLocations[g2].y)
+            const minY = Math.min(galaxyLocations[g1].y, galaxyLocations[g2].y)
+            const expX = emptyRowsColumns.emptyColumns.filter(q=>q>minX&&q<maxX).length
+            const expY = emptyRowsColumns.emptyColumns.filter(q=>q>minY&&q<maxY).length
+            shortestPaths.push(Math.abs(galaxyLocations[g2].x-galaxyLocations[g1].x)+Math.abs(galaxyLocations[g2].y-galaxyLocations[g1].y)+((expY+expX)*expFact))
         }
     }
     return shortestPaths
 }
 
 const grid = parseInput(data)
-// for (const row of grid) {
-//     console.log(row)
-// }
-const expanded = expandUniverse(grid)
-// for (const row of expanded) {
-//     console.log(row)
-// }
-const galaxyLocations = findGalaxies(expanded)
-// console.log(galaxyLocations)
-shortestPaths = findShortestPaths(galaxyLocations)
-console.log(shortestPaths)
+
+const emptyRowsColumns = expandUniverse(grid)
+const galaxyLocations = findGalaxies(grid)
+console.log(emptyRowsColumns)
+console.log(galaxyLocations)
+
+shortestPaths = findShortestPaths(galaxyLocations, emptyRowsColumns)
+
+
 const SOLUTION_1 = shortestPaths.reduce((a,c)=>a+c)
 
 module.exports = {}
