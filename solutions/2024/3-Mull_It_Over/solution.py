50240bf1c54b63e683abeca827f4f6977efb3afe
pkaodev <polsson.development@gmail.com>
2024-12-03 17:29:29 +0000

    d3

--- solutions/2024/3-Mull_It_Over/solution.py
+++ solutions/2024/3-Mull_It_Over/solution.py
@@ -1,89 +1,99 @@
 import os
+import re
 current_dir = os.path.dirname(os.path.realpath(__file__))
+# input_path = os.path.join(os.path.dirname(__file__), 'input')
 input_path = os.path.join(os.path.dirname(__file__), 'input')
 
 with open(input_path, 'r', encoding='utf8') as file:
     data = file.read().split('\n')
-    data = data[0] if len(data) is 1 else data
+    data = ''.join(data)
     
-print(data)
+def solution_1(data):
+	solution = 0
+	_i1 = None
+	_i2 = None
+	for i in range(0, len(data)):
+		if data[i] == 'm':
+			_i1 = i
+		if data[i] == ')':
+			_i2 = i
 
-# q1
-# num unique coords visited
+		if _i1 is not None and _i2 is not None:
+			print(data[_i1:_i2+1])
+			_extracted_vals = extract_vals(data[_i1:_i2+1])
+			if _extracted_vals is not None:
+				solution += _extracted_vals[0] * _extracted_vals[1]
+				_i1 = None
+				_i2 = None
 
+	print(solution)
 
+def extract_vals(exp):
+	my_regex = r'mul\((\d{1,3}),(\d{1,3})\)'
+	match = re.match(my_regex, exp)
+	if match:
+		return (int(match.group(1)), int(match.group(2)))
+	else:
+		return None
 
+def extract_do_dont(exp):
+	do_regex = r'do\(\)'
+	dont_regex = r"don't\(\)"
 
-def q1_1(data):
-	x = 0
-	y = 0
-	visited = {'0,0',}
+	do_match = re.match(do_regex, exp)
+	dont_match = re.match(dont_regex, exp)
+
+	if do_match:
+		return "do"
+	elif dont_match:
+		return "dont"
+	else:
+		return None
+
+def solution_2(data):
+	solution = 0
+	do_enabled = True
  
-	def up():
-		nonlocal y
-		y += 1
-  
-	def down():
-		nonlocal y
-		y -= 1
-  
-	def right():
-		nonlocal x
-		x += 1
-  
-	def left():
-		nonlocal x
-		x -= 1
-  
-	move = {
-		'^': up,
-		'v': down,
-		'>': right,
-		'<': left
-	}
+	# mul values
+	_i1 = None
+	_i2 = None
  
-	for d in data:
-		move[d]()
-		visited.add(f"{x},{y}")
-  
-	return len(visited)
-
-# q2
-def q2_1(data):
-
-	visited = {'0,0',}
-  
-	move = {
-			'^': ('y',1),
-			'v': ('y',-1),
-			'>': ('x',1),
-			'<': ('x',-1),
-	}
+	# do/dont
+	_i3 = None
+	_i4 = None
  
-	coords = {
-		'santa':{
-			'x': 0,
-			'y': 0
-		},
-		'robo':{
-			'x': 0,
-			'y': 0
-		},
-	}
- 
- 
-	curr = 'santa'
+	for i in range(0, len(data)):
+		if data[i] == 'm':
+			_i1 = i
+		if data[i] == ')':
+			_i2 = i
 
-	for d in data:
-		curr = 'santa' if curr == 'robo' else 'robo'
+		if data[i] == 'd':
+			_i3 = i
+		if data[i] == ')':
+			_i4 = i
 
-		direction, value = move[d]
-		
-		coords[curr][direction] += value
-  
-		visited.add(f"{coords[curr]['x']},{coords[curr]['y']}")
-		
-  
-	return len(visited)
+		if _i3 is not None and _i4 is not None:
+			_extracted_vals = extract_do_dont(data[_i3:_i4+1])
+			if _extracted_vals is not None:
+				if _extracted_vals == "do":
+					do_enabled = True
+				else:
+					do_enabled = False
+				_i3 = None
+				_i4 = None
 
-print(q2_1(data))
+		if _i1 is not None and _i2 is not None:
+			_extracted_vals = extract_vals(data[_i1:_i2+1])
+			if _extracted_vals is not None:
+				if do_enabled:
+					solution += _extracted_vals[0] * _extracted_vals[1]
+				_i1 = None
+				_i2 = None
+
+
+	print(solution)
+
+
+# solution_1(data)
+solution_2(data)
